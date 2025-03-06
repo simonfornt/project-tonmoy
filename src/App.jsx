4ab77cf2550d9ae18ba8a6792e5ed4ff6d29dd23
simonfotnt <simonpiar98@gamil.com>
2025-03-07 00:11:10 +0600

    added donate card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './component/Nav'
 import Hero from './component/Hero'
 import About from './component/About'
+import DonationCard from './component/Cards'
 
 import './App.css'
 
@@ -12,6 +13,11 @@ function App() {
      <Navbar/>
      <Hero/>
      <About/>
+     <div className='md:flex gap-3 mx-10 justify-between'>
+     <DonationCard/>
+     <DonationCard/>
+     <DonationCard/>
+     </div>
      
     
     </>
